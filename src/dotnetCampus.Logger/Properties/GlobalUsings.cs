1bc17c3fc45b50b31b3fcc28c0ea987f34e74226
walterlv <xblero@gmail.com>
2024-05-07 16:20:57 +0800

    尽量避免 net8 的写法在其他框架写不出来的问题

---
+++ src/dotnetCampus.Logger/Properties/GlobalUsings.cs
@@ -0,0 +1,18 @@
+﻿global using dotnetCampus.Logging.Properties;
+
+// .NET 8.0 or later
+#if NET8_0_OR_GREATER
+global using System.Collections.Immutable;
+global using ImmutableArrayILogger = System.Collections.Immutable.ImmutableArray<dotnetCampus.Logging.ILogger>;
+global using ImmutableHashSetString = System.Collections.Immutable.ImmutableHashSet<string>;
+#else
+global using ImmutableArrayILogger = System.Collections.Generic.List<dotnetCampus.Logging.ILogger>;
+global using ImmutableHashSetString = System.Collections.Generic.HashSet<string>;
+#endif
+
+// .NET 6.0 or later
+#if NET6_0_OR_GREATER
+global using Math = System.Math;
+#else
+global using Math = dotnetCampus.Logging.Properties.Compatibility;
+#endif
