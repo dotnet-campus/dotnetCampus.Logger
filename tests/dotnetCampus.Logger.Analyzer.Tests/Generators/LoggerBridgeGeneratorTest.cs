3bf197349df55463d418a86638c69db0ba211079
lindexi <lindexi_gd@163.com>
2024-09-18 16:15:11 +0800

    尝试加上单元测试

---
+++ tests/dotnetCampus.Logger.Analyzer.Tests/Generators/LoggerBridgeGeneratorTest.cs
@@ -0,0 +1,53 @@
+﻿using dotnetCampus.Logger.Generators;
+
+using Microsoft.CodeAnalysis.CSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace dotnetCampus.Logger.Analyzer.Tests.Generators;
+
+[TestClass]
+public class LoggerBridgeGeneratorTest
+{
+    [TestMethod]
+    public void TestGenerateLoggerBridge()
+    {
+        var loggerBridgeGenerator = new LoggerBridgeGenerator();
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(loggerBridgeGenerator);
+
+        var compilation = CSharpCompilation.Create
+        (
+            "Test",
+            syntaxTrees: 
+            [
+                CSharpSyntaxTree.ParseText
+                (
+                    """
+                    using System;
+                    using System.Diagnostics;
+                    using System.Threading;
+                    using System.Threading.Tasks;
+                    using dotnetCampus.Logging.Attributes;
+                    using dotnetCampus.Logging.Configurations;
+                    using dotnetCampus.Logging.Writers;
+                    
+                    namespace LoggerSample.MainApp;
+                    
+                    [ImportLoggerBridge<global::LoggerSample.LoggerIndependentLibrary.Logging.ILoggerBridge>]
+                    [ImportLoggerBridge<global::LoggerSample.LoggerIndependentProject.Logging.ILoggerBridge>]
+                    internal partial class LoggerBridgeLinker;
+                    """
+                )
+            ]
+        );
+
+        driver = driver.RunGenerators(compilation);
+        var result = driver.GetRunResult();
+        Assert.IsNotNull(result);
+    }
+}
