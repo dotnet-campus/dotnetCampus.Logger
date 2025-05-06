aa11f9663bfbe96ef70e6f156a5cfe2cae523f7f
walterlv <xblero@gmail.com>
2025-05-06 11:41:05 +0800

    在输出源时，附带教程

--- src/DotNetCampus.Logger.Analyzer/Generators/LoggerGenerator.cs
+++ src/DotNetCampus.Logger.Analyzer/Generators/LoggerGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using DotNetCampus.Logger.Utils.CodeAnalysis;
 using DotNetCampus.Logger.Utils.IO;
@@ -35,73 +37,113 @@ public class LoggerGenerator : IIncrementalGenerator
             return;
         }
 
-
+        // 查找所有的 Log 类型（这只是一个代表，用于检测是否有引用的程序集包含即将生成的类型）。
         var logTypes = compilation.GetTypesByMetadataName("DotNetCampus.Logging.Log")
             .Where(x => x.DeclaredAccessibility is Accessibility.Public || x.ContainingAssembly.GivesAccessTo(compilation.Assembly))
             .ToArray();
+        // 查找 Log 类型所在的所有程序集。
         var assemblies = logTypes
             .Select(x => x.ContainingAssembly.Name)
             .ToArray();
-        var isTypeVisible = logTypes.Length is 1;
-
-        var sourceFiles = EmbeddedSourceFiles.Enumerate("Assets/Sources").ToArray();
-
-        foreach (var file in sourceFiles)
+        // 检查 Log 类型的来源。
+        var logVisibleType = (assemblies, logTypes.Length) switch
         {
-            var originalTypeFullType = $"{file.Namespace.Replace("DotNetCampus.Logger", "DotNetCampus.Logging")}.{file.TypeName}";
-            var isLoggerBridge = originalTypeFullType == "DotNetCampus.Logging.Bridges.ILoggerBridge";
-            var generatedTypeFullName = isLoggerBridge ? $"{rootNamespace}.Logging.{file.TypeName}" : originalTypeFullType;
+            (["DotNetCampus.Logger"], _) => LogVisibleType.ReferenceCompile,
+            (_, 0) => LogVisibleType.Invisible,
+            (_, 1) => LogVisibleType.InternalsVisibleToSource,
+            (_, _) when assemblies.Contains("DotNetCampus.Logger") => LogVisibleType.ConflictBetweenReferenceCompileAndInternalsVisibleToSource,
+            (_, _) => LogVisibleType.ConflictAmongInternalsVisibleToSources,
+        };
 
+        foreach (var file in EmbeddedSourceFiles.Enumerate("Assets/Sources"))
+        {
             var code = GenerateSource(file.TypeName, rootNamespace, file.Content);
-
-            code = (useGeneratedLogger, isLoggerBridge, isTypeDefined: isTypeVisible) switch
-            {
-                (true, true, _) => $"""
-// DCUseGeneratedLogger 被设置为 true，以下为生成的 ILoggerBridge 类型。
-// ILoggerBridge 使用 DCGeneratedLoggerNamespace 设置的命名空间 {rootNamespace}。
-
-{code}
-""",
-                (true, false, true) => $"""
-// 此文件所涉及的类型 {originalTypeFullType} 已经在依赖程序集中定义，并被传递给当前程序集；为避免冲突，当前程序集不再生成 {generatedTypeFullName} 类型。
-{string.Join("\n", assemblies.Select(x => $"//  - {(x == "DotNetCampus.Logger" ? x : $"{x} (通过 InternalsVisibleTo 传递)")}"))}
-
-/*
-{code}
-*/
-""",
-                (true, false, false) => $"""
-// DCUseGeneratedLogger 被设置为 true，以下为生成的 {generatedTypeFullName} 类型。
-
-{code}
-""",
-                (false, true, _) => $"""
-// 因为 DCUseGeneratedLogger 未被设置为 true，所以不会生成 {generatedTypeFullName} 类型。
-
-/*
-{code}
-*/
-""",
-                (false, false, true) => $"""
-// 因为 DCUseGeneratedLogger 未被设置为 true，所以不会生成 {generatedTypeFullName} 类型。
-// 此文件所涉及的类型已经在依赖程序集中定义，并被传递给当前程序集。
-{string.Join("\n", assemblies.Select(x => $"//  - {(x == "DotNetCampus.Logger" ? x : $"{x} (通过 InternalsVisibleTo 传递)")}"))}
-
-/*
-{code}
-*/
-""",
-                (false, false, false) => $"""
-// 因为 DCUseGeneratedLogger 未被设置为 true，所以不会生成 {generatedTypeFullName} 类型。
-
-/*
-{code}
-*/
-""",
-            };
-
-            context.AddSource($"{file.TypeName}.g.cs", SourceText.From(code, Encoding.UTF8));
+            var dependentCode = WrapSource(file, code, logVisibleType, assemblies);
+            context.AddSource($"{file.TypeName}.g.cs", SourceText.From(dependentCode, Encoding.UTF8));
         }
+    }
+
+    private string WrapSource(EmbeddedSourceFile file, string code, LogVisibleType visibleType, IReadOnlyList<string> assemblies)
+    {
+        var isLoggerBridge = file.TypeName is "ILoggerBridge";
+        return visibleType switch
+        {
+            LogVisibleType.Invisible => code,
+            LogVisibleType.ReferenceCompile => $"""
+// 因为已引用了 DotNetCampus.Logger，所以不会生成 {file.TypeName} 类型。
+// 如果希望使用源生成器，从而不额外引入依赖，请按如下方法设置你的包引用：
+//
+//   <PackageReference Include="DotNetCampus.Logger" PrivateAssets="all" ExcludeAssets="compile;runtime" />
+//
+// 其中：
+//  1. PrivateAssets="all" 用于表明生成的 NuGet 包将不引入 DotNetCampus.Logger 包依赖；
+//  2. ExcludeAssets 中 compile 用于不引入编译时的依赖（此时以下源才会自动解除注释）；
+//  3. ExcludeAssets 中 runtime 用于不将依赖复制到目标目录（因为运行时已经不需要它们了）。
+
+/*
+{code}*/
+
+""",
+            LogVisibleType.InternalsVisibleToSource => isLoggerBridge
+                ? $"""
+// 因为日志类型已通过此项目的 InternalsVisibleTo 传递给了该项目，所以只生成 {file.TypeName} 这一个类型用于桥接。
+{string.Join("\n", assemblies.Select(x => $"//  - {x} (通过 InternalsVisibleTo 传递)"))}
+
+{code}
+"""
+                : $"""
+// 因为日志类型已通过此项目的 InternalsVisibleTo 传递给了该项目，所以不会生成 {file.TypeName} 类型。
+{string.Join("\n", assemblies.Select(x => $"//  - {x} (通过 InternalsVisibleTo 传递)"))}
+
+/*
+{code}*/
+
+""",
+            LogVisibleType.ConflictAmongInternalsVisibleToSources => isLoggerBridge
+                ? $"""
+// 日志类型已通过以下多个项目的 InternalsVisibleTo 传递给了该项目，生成 {file.TypeName} 类型用于桥接。
+{string.Join("\n", assemblies.Select(x => $"//  - {x} (通过 InternalsVisibleTo 传递)"))}
+
+{code}
+"""
+                : $"""
+// 日志类型已通过以下多个项目的 InternalsVisibleTo 传递给了该项目，出现了类型冲突，所以会强制生成 {file.TypeName} 类型以解决冲突。
+{string.Join("\n", assemblies.Select(x => $"//  - {x} (通过 InternalsVisibleTo 传递)"))}
+
+// 如果希望消除编译时的冲突警告，请在项目中添加以下属性：
+//
+//   <PropertyGroup>
+//     <!-- 类型冲突，将强制使用本项目中生成的源。 -->
+//     <NoWarn>$(NoWarn);CS0436</NoWarn>
+//   </PropertyGroup>
+
+#pragma warning disable CS0436
+
+{code}
+""",
+            LogVisibleType.ConflictBetweenReferenceCompileAndInternalsVisibleToSource => $"""
+// # 类型冲突，项目无法编译
+//
+// ## 原因
+//
+// 同时引用了日志库和包含日志源生成器且设置了 InternalsVisibleTo 的项目：
+{string.Join("\n", assemblies.Select(x => x == "DotNetCampus.Logger" ? $"//  - {x} (引用)" : $"//  - {x} (通过 InternalsVisibleTo 传递)"))}
+//
+// ## 解决方法
+//
+// 任选其一：
+//
+// 1. 全部使用引用，而不是源生成器。
+//    即删除 ExcludeAssets="compile;runtime"
+//    <PackageReference Include="DotNetCampus.Logger" />
+// 2. 删除到此项目的 InternalsVisibleTo
+
+/*
+{code}*/
+
+""",
+            _ => throw new ArgumentOutOfRangeException(nameof(visibleType), visibleType, null),
+        };
     }
 
     private string GenerateSource(string typeName, string rootNamespace, string sourceText)
@@ -127,5 +169,33 @@ public class LoggerGenerator : IIncrementalGenerator
 
         // 此类型是 public 的，需要修改为 internal。
         return sourceText.ReplaceTypeModifier("internal");
+    }
+
+    private enum LogVisibleType
+    {
+        /// <summary>
+        /// 日志类型未引用，或不可见。
+        /// </summary>
+        Invisible,
+
+        /// <summary>
+        /// 日志类型通过库直接引用。
+        /// </summary>
+        ReferenceCompile,
+
+        /// <summary>
+        /// 日志类型在引用的项目中已生成，并通过 InternalsVisibleTo 传递了进来。
+        /// </summary>
+        InternalsVisibleToSource,
+
+        /// <summary>
+        /// 日志类型通过多个项目的 InternalsVisibleTo 传递了进来。
+        /// </summary>
+        ConflictAmongInternalsVisibleToSources,
+
+        /// <summary>
+        /// 日志类型既通过库直接引用，又通过 InternalsVisibleTo 传递了进来；此时必然会冲突，导致编译失败。
+        /// </summary>
+        ConflictBetweenReferenceCompileAndInternalsVisibleToSource,
     }
 }
